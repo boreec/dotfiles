438fa7b7a5a3e7f3b7a768db865ca13dd1816622
Cyprien Boree <cyprien.boree@tuta.io>
2024-09-11 17:10:55 +0900

    feat: add colorcolumns in nvim

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -4,5 +4,6 @@ vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
 vim.cmd("set number")
-vim.g.mapleader = " "
+vim.api.nvim_set_option_value("colorcolumn", "80,100", {})
+
 
