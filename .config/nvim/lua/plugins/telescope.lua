5e8f6e8176712919e5d89b66c0f4cfb60131b6cd
Cyprien Boree <cyprien.boree@tuta.io>
2024-09-12 00:56:34 +0900

    chore: move lsp binds to use telescope plugin

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -5,6 +5,10 @@ return {
     dependencies = { 'nvim-lua/plenary.nvim' },
     config = function()
       local builtin = require("telescope.builtin")
+      vim.keymap.set('n', 'gd', builtin.lsp_definitions, {})
+      vim.keymap.set('n', 'gi', builtin.lsp_implementations, {})
+      vim.keymap.set('n', '<leader>d', builtin.diagnostics, {})
+      vim.keymap.set('n', '<leader>s', builtin.lsp_document_symbols, {})
       vim.keymap.set('n', '<C-p>', builtin.find_files, {})
       vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
       vim.keymap.set('n', 'gr', builtin.lsp_references, {})
