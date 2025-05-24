22df0114e52828bc93510e8723d4844dc3cc11c1
Cyprien Boree <cyprien.boree@tuta.io>
2025-05-24 14:00:20 +0900

    feat: add fish config

---
+++ .config/fish/fish/config.fish
@@ -0,0 +1,41 @@
+set -U fish_greeting
+set -gx EDITOR vim
+set -gx GOPATH $HOME/go
+set -gx TERM xterm-256color
+
+fish_add_path $GOPATH/bin
+
+if status is-interactive
+    if not set -q TMUX
+        exec tmux
+    end
+end
+
+mise activate fish | source
+starship init fish | source
+zoxide init fish | source
+
+alias cd        'z'
+alias dofus     '~/Ankama\ Launcher-Setup-x86_64.AppImage'
+alias ga        'git add'
+alias gap       'git add -p'
+alias gc        'git commit'
+alias gcm       'git commit -m'
+alias gd        'git diff'
+alias gds       'git diff --staged'
+alias gs        'git status'
+alias gpl       'git pull'
+alias gps       'git push'
+alias gcleanbr  'git branch -d \
+  (git branch -l "chore/*") \
+  (git branch -l "docs/*") \
+  (git branch -l "feat/*") \
+  (git branch -l "feature/*") \
+  (git branch -l "fix/*") \
+  (git branch -l "hotfix/*") \
+  2>/dev/null || echo "no branches to clean"'
+alias ls        'ls --color=auto'
+alias ll        'ls --color=auto -l'
+alias n         'nvim'
+alias prc       'pre-commit'
+alias tmux      'tmux -f ~/.config/tmux/tmux.conf'
