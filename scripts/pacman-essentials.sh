18a328a37c6b486fe95a2397b6da4cfeaa17ea06
Cyprien Boree <cyprien.boree@tuta.io>
2025-12-25 16:53:31 +0900

    chore: update pacman-essentials

--- scripts/pacman-essentials.sh
+++ scripts/pacman-essentials.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 sudo pacman -S blueman bluez curl dolphin efibootmgr fd firefox fzf keepassxc \
   kitty go hyprland jq lua luarocks ly lz4 nvim perl pip python stylua tmux \
-  tree-sitter-cli ttf-jetbrains-mono-nerd vlc waybar wget wl-clipboard
+  tree tree-sitter-cli ttf-jetbrains-mono-nerd vlc waybar wget wl-clipboard
