c251f5e094e122811e89dcb2412d880a95ed3aa3
Cyprien Boree <cyprien.boree@tuta.io>
2025-12-26 23:10:29 +0900

    chore: update pacman essentials

--- scripts/pacman-essentials.sh
+++ scripts/pacman-essentials.sh
@@ -3,4 +3,4 @@ sudo pacman -S acpica blueman bluez curl dmidecodee dolphin efibootmgr efivar \
   fd firefox fzf fwupd git github-cli keepassxc kitty go hyprland jq less lua \
   luarocks ly lz4 man-db more most nvim perl pip python stylua tokei tmux tree \
   tree-sitter-cli ttf-jetbrains-mono-nerd vlc vlc-plugins-all waybar wget \
-  wl-clipboard
+  wl-clipboard zoxide
